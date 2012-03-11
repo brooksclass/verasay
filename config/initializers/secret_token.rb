5593995d7c0040365a74a5330dfc78a1c10cfb55
Brooks Childers <brooks.childers@sitmybaby.com>
2012-03-11 11:23:23 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Verasay::Application.config.secret_token = '58876e04e636b1eb843193eaa0ba07efa0d79e7c8007a5c2de1c49ff9753c8a54912e7c069485737c3c9af9f9ec7f994f778570f7ba63deb2154ba4a6aa94c3c'
